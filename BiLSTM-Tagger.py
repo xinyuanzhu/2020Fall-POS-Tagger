b30e321ef141ac0551e19848c6246ae7e34a295d
xinyuanzhu <zhuxy.ustc@gmail.com>
2020-11-19 08:41:43 +0800

    add gitignore; update BiLSTM-Tagger

--- BiLSTM-Tagger.py
+++ BiLSTM-Tagger.py
@@ -9,7 +9,7 @@ import numpy as np
 
 import time
 import random
-import os 
+import os
 
 
 SEED = 2020
@@ -19,15 +19,38 @@ np.random.seed(SEED)
 torch.manual_seed(SEED)
 # torch.backends.cudnn.deterministic = True
 
-TEXT = data.Field(lower = True)
-UD_TAGS = data.Field(unk_token = None)
-PTB_TAGS = data.Field(unk_token = None)
+TEXT = data.Field(lower=True)
+UD_TAGS = data.Field(unk_token=None)
+PTB_TAGS = data.Field(unk_token=None)
 
-fields = (("text", TEXT), ("udtags", UD_TAGS), ("ptbtags", PTB_TAGS))
+fields = (("text", TEXT), ("udtags", UD_TAGS))
 
 train_data, valid_data, test_data = datasets.UDPOS.splits(fields)
 print(f"Number of training examples: {len(train_data)}")
 print(f"Number of validation examples: {len(valid_data)}")
 print(f"Number of testing examples: {len(test_data)}")
 
-print()
+# print(type(train_data))
+# <class 'torchtext.datasets.sequence_tagging.UDPOS'>
+
+# print(vars(train_data.examples[0]))
+# {'text': ['al', '-', 'zaman', ':', 'american', 'forces', 'killed', 'shaikh',
+# 'abdullah', 'al', '-', 'ani', ',', 'the', 'preacher', 'at', 'the', 'mosque',
+# 'in', 'the', 'town', 'of', 'qaim', ',', 'near', 'the', 'syrian', 'border',
+# '.'], udtags': ['PROPN', 'PUNCT', 'PROPN', 'PUNCT', 'ADJ', 'NOUN', 'VERB',
+# 'PROPN', PROPN', 'PROPN', 'PUNCT', 'PROPN', 'PUNCT', 'DET', 'NOUN', 'ADP',
+# 'DET', 'NOUN', 'ADP', 'DET', 'NOUN', 'ADP', 'PROPN', 'PUNCT', 'ADP', 'DET',
+# 'ADJ', 'NOUN', 'PUNCT']}
+
+MIN_FREQ = 2
+
+TEXT.build_vocab(train_data,
+                 min_freq=MIN_FREQ,
+                 vectors="glove.6B.100d",
+                 unk_init=torch.Tensor.normal_)
+
+
+UD_TAGS.build_vocab(train_data)
+
+print(f"Unique tokens in TEXT vocabulary: {len(TEXT.vocab)}")
+print(f"Unique tokens in UD_TAG vocabulary: {len(UD_TAGS.vocab)}")
