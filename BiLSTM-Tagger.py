589bd61d032344884aa32d80a8198eb7198069b3
xinyuanzhu <zhuxy.ustc@gmail.com>
2020-11-23 15:29:02 +0800

    upload training log&model ckpt; update BiLSTM-Tagger

--- BiLSTM-Tagger.py
+++ BiLSTM-Tagger.py
@@ -46,8 +46,8 @@ MIN_FREQ = 2
 
 TEXT.build_vocab(train_data,
                  min_freq=MIN_FREQ,
-                 # vectors="glove.6B.100d",
-                 # unk_init=torch.Tensor.normal_
+                 vectors="glove.6B.100d",
+                 unk_init=torch.Tensor.normal_
                  )
 
 # NEED .vector_cache/glove.6B.zip
@@ -139,3 +139,151 @@ model = BiLSTMPOSTagger(INPUT_DIM,
                         BIDIRECTIONAL,
                         DROPOUT,
                         PAD_IDX)
+
+
+def init_weights(m):
+    for name, param in m.named_parameters():
+        nn.init.normal_(param.data, mean=0, std=0.1)
+
+
+model.apply(init_weights)
+
+
+def count_parameters(model):
+    return sum(p.numel() for p in model.parameters() if p.requires_grad)
+
+
+print(f'The model has {count_parameters(model):,} trainable parameters')
+pretrained_embeddings = TEXT.vocab.vectors
+
+print(pretrained_embeddings.shape)
+
+model.embedding.weight.data.copy_(pretrained_embeddings)
+
+optimizer = optim.Adam(model.parameters())
+
+
+TAG_PAD_IDX = UD_TAGS.vocab.stoi[UD_TAGS.pad_token]
+
+criterion = nn.CrossEntropyLoss(ignore_index=TAG_PAD_IDX)
+
+
+def categorical_accuracy(preds, y, tag_pad_idx):
+    """
+    Returns accuracy per batch, i.e. if you get 8/10 right, this returns 0.8, NOT 8
+    """
+    max_preds = preds.argmax(
+        dim=1, keepdim=True)  # get the index of the max probability
+    non_pad_elements = (y != tag_pad_idx).nonzero()
+    correct = max_preds[non_pad_elements].squeeze(1).eq(y[non_pad_elements])
+    return correct.sum() / torch.FloatTensor([y[non_pad_elements].shape[0]])
+
+
+def train(model, iterator, optimizer, criterion, tag_pad_idx):
+
+    epoch_loss = 0
+    epoch_acc = 0
+
+    model.train()
+
+    for batch in iterator:
+
+        text = batch.text
+        tags = batch.udtags
+
+        optimizer.zero_grad()
+
+        # text = [sent len, batch size]
+
+        predictions = model(text)
+
+        # predictions = [sent len, batch size, output dim]
+        # tags = [sent len, batch size]
+
+        predictions = predictions.view(-1, predictions.shape[-1])
+        tags = tags.view(-1)
+
+        # predictions = [sent len * batch size, output dim]
+        # tags = [sent len * batch size]
+
+        loss = criterion(predictions, tags)
+
+        acc = categorical_accuracy(predictions, tags, tag_pad_idx)
+
+        loss.backward()
+
+        optimizer.step()
+
+        epoch_loss += loss.item()
+        epoch_acc += acc.item()
+
+    return epoch_loss / len(iterator), epoch_acc / len(iterator)
+
+
+def evaluate(model, iterator, criterion, tag_pad_idx):
+
+    epoch_loss = 0
+    epoch_acc = 0
+
+    model.eval()
+
+    with torch.no_grad():
+
+        for batch in iterator:
+
+            text = batch.text
+            tags = batch.udtags
+
+            predictions = model(text)
+
+            predictions = predictions.view(-1, predictions.shape[-1])
+            tags = tags.view(-1)
+
+            loss = criterion(predictions, tags)
+
+            acc = categorical_accuracy(predictions, tags, tag_pad_idx)
+
+            epoch_loss += loss.item()
+            epoch_acc += acc.item()
+
+    return epoch_loss / len(iterator), epoch_acc / len(iterator)
+
+
+def epoch_time(start_time, end_time):
+    elapsed_time = end_time - start_time
+    elapsed_mins = int(elapsed_time / 60)
+    elapsed_secs = int(elapsed_time - (elapsed_mins * 60))
+    return elapsed_mins, elapsed_secs
+
+
+N_EPOCHS = 10
+
+best_valid_loss = float('inf')
+
+for epoch in range(N_EPOCHS):
+
+    start_time = time.time()
+
+    train_loss, train_acc = train(
+        model, train_iterator, optimizer, criterion, TAG_PAD_IDX)
+    valid_loss, valid_acc = evaluate(
+        model, valid_iterator, criterion, TAG_PAD_IDX)
+
+    end_time = time.time()
+
+    epoch_mins, epoch_secs = epoch_time(start_time, end_time)
+
+    if valid_loss < best_valid_loss:
+        best_valid_loss = valid_loss
+        torch.save(model.state_dict(), 'tut1-model.pt')
+
+    print(f'Epoch: {epoch+1:02} | Epoch Time: {epoch_mins}m {epoch_secs}s')
+    print(f'\tTrain Loss: {train_loss:.3f} | Train Acc: {train_acc*100:.2f}%')
+    print(f'\t Val. Loss: {valid_loss:.3f} |  Val. Acc: {valid_acc*100:.2f}%')
+    
+    
+model.load_state_dict(torch.load('tut1-model.pt'))
+
+test_loss, test_acc = evaluate(model, test_iterator, criterion, TAG_PAD_IDX)
+
+print(f'Test Loss: {test_loss:.3f} |  Test Acc: {test_acc*100:.2f}%')
